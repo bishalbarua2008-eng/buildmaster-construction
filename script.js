617eb82292990f6d42ab46cad35122d1571e4a3d
bishalbarua2008-eng <bishalbarua2008@gmail.com>
2026-07-03 11:50:40 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Back To Top Button
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        topBtn.style.display = "block";
+
+    }else{
+
+        topBtn.style.display = "none";
+
+    }
+
+});
+
+topBtn.onclick = () =>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+};
