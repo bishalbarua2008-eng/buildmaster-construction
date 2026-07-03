f647b297246c0a33c80c151ad820fd1eb3aa8c84
bishalbarua2008-eng <bishalbarua2008@gmail.com>
2026-07-03 16:47:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ topBtn.onclick = () =>{
     });
 
 };
+// =========================
+// HEADER SCROLL EFFECT
+// =========================
+
+window.addEventListener("scroll", function () {
+
+    const header = document.querySelector(".header");
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+});
